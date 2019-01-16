d115c21bd759ce3638432d289f954d9871adbe77
Sushil Singh <software.shiel@outlook.in>
2019-01-17 01:01:31 +0530

    Initail Commit

---
+++ CustomAuthentication/Authentication/CustomMembership.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using CustomAuthentication.Models;
+
+namespace CustomAuthentication.Authentication
+{
+    public class CustomMembership : MembershipProvider
+    {
+        public override bool EnablePasswordRetrieval => throw new NotImplementedException();
+
+        public override bool EnablePasswordReset => throw new NotImplementedException();
+
+        public override bool RequiresQuestionAndAnswer => throw new NotImplementedException();
+
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override int MaxInvalidPasswordAttempts => throw new NotImplementedException();
+
+        public override int PasswordAttemptWindow => throw new NotImplementedException();
+
+        public override bool RequiresUniqueEmail => throw new NotImplementedException();
+
+        public override MembershipPasswordFormat PasswordFormat => throw new NotImplementedException();
+
+        public override int MinRequiredPasswordLength => throw new NotImplementedException();
+
+        public override int MinRequiredNonAlphanumericCharacters => throw new NotImplementedException();
+
+        public override string PasswordStrengthRegularExpression => throw new NotImplementedException();
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            using (AuthenticationDbContext dbContext = new AuthenticationDbContext())
+            {
+                var user = (from us in dbContext.Users
+                            where string.Compare(username, us.Username, StringComparison.OrdinalIgnoreCase) == 0
+                            select us).FirstOrDefault();
+
+                if (user == null)
+                {
+                    return null;
+                }
+                var selectedUser = new CustomMembershipUser(user);
+
+                return selectedUser;
+            }
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            using (AuthenticationDbContext dbContext = new AuthenticationDbContext())
+            {
+                string username = (from u in dbContext.Users
+                                   where string.Compare(email, u.Email) == 0
+                                   select u.Username).FirstOrDefault();
+
+                return !string.IsNullOrEmpty(username) ? username : string.Empty;
+            }
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+
+            using (AuthenticationDbContext dbContext = new AuthenticationDbContext())
+            {
+                var user = (from us in dbContext.Users
+                            where string.Compare(username, us.Username, StringComparison.OrdinalIgnoreCase) == 0
+                            && string.Compare(password, us.Password, StringComparison.OrdinalIgnoreCase) == 0
+                            && us.IsActive == true
+                            select us).FirstOrDefault();
+
+                return (user != null) ? true : false;
+            }
+        }
+    }
+}
---
+++ CustomAuthentication/Models/AuthenticationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Configuration;
+
+namespace CustomAuthentication.Models
+{
+    public class AuthenticationDbContext : DbContext
+    {
+        public AuthenticationDbContext()
+            : base("AuthenticationDbContext") {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Roles)
+                .WithMany(r => r.Users)
+                .Map(m =>
+                {
+                    m.ToTable("UserRoles");
+                    m.MapLeftKey("UserId");
+                    m.MapRightKey("RoleId");
+                });
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
